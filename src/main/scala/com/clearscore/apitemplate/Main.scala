c30888f9d1a76230726d3f8f620da93b069f5c3d
Harvey Cambridge <harvey.cambridge@clearscore.com>
2024-01-26 21:24:55 +0000

    Create example service with routes

--- src/main/scala/com/clearscore/apitemplate/Main.scala
+++ src/main/scala/com/clearscore/apitemplate/Main.scala
@@ -1,9 +1,11 @@
 package com.clearscore.apitemplate
 
 import cats.effect.*
-import com.clearscore.apitemplate.http.Routes
-import org.http4s._
-import org.http4s.implicits._
+import com.clearscore.apitemplate.db.ExampleRepositoryImpl
+import com.clearscore.apitemplate.http.{ExampleRoutes, Routes}
+import com.clearscore.apitemplate.service.{ExampleService, ExampleServiceImpl}
+import org.http4s.*
+import org.http4s.implicits.*
 import org.http4s.jetty.server.JettyBuilder
 import org.http4s.server.Router
 
@@ -13,12 +15,18 @@ object Main extends IOApp {
 
   private val routes: Routes[IO] = new Routes[IO]
 
+  private val exampleRepository = new ExampleRepositoryImpl
+  private val exampleService = new ExampleServiceImpl(exampleRepository)
+  private val exampleRoutes = new ExampleRoutes(exampleService)
+
   override def run(args: List[String]): IO[ExitCode] = {
 
     val apis = Router(
       "/" -> routes.testRoute1,
-      "/" -> routes.testRoute2
+      "/" -> routes.testRoute2,
+      "/example" -> exampleRoutes.routes
     )
+
     buildServer(apis)as(ExitCode.Success)
   }
 
